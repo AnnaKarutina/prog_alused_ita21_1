73143f94dbd64b1d60deafa2dc3cfc403177edea
Anna Karutina <anna.karutina@khk.ee>
2021-10-21 11:06:54 +0300

    1.2 President - kasutaja sisendiga

---
+++ praks1/1.2 President + sisend.py
@@ -0,0 +1,8 @@
+# defineerin muutujad ja omistan väärtused
+aasta = int(input("Sisesta aasta väärtus taisarvuna: "))
+president = input("Sisesta presidendi nimi: ")
+lause_keskosa = ". aastal valiti Eesti presidendiks "
+
+# muutujate kasutamine
+lause = str(aasta) + lause_keskosa + president
+print(lause)
